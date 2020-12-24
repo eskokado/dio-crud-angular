0b8d05ee38b6f9e8cb20ef38a9fa07235cf368fc
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-24 08:47:04 -0300

    Componentizando nossos inputs

--- src/app/filmes/filmes.module.ts
+++ src/app/filmes/filmes.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
-import { MaterialModule } from '../shared/material/material.module';
 import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.component';
 
+import { MaterialModule } from '../shared/material/material.module';
+import { CamposModule } from './../shared/components/campos/campos.module';
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
     ReactiveFormsModule,
     FormsModule,
+    CamposModule
   ],
   declarations: [CadastroFilmesComponent, ListagemFilmesComponent]
 })
