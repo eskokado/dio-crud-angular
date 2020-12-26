1898654048c3df2b3da409ab47cb1274bbcba056
Edson Shideki Kokado <eskokado@gmail.com>
2020-12-26 07:27:48 -0300

    Implementando ordenação e filtragem

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -16,6 +16,8 @@ export class ListagemFilmesComponent implements OnInit {
   filmes: Filme[] = [];
   filtrosListagem: FormGroup;
   generos: Array<string>;
+  texto: string;
+  genero: string;
 
   constructor(
     private filmesService: FilmesService,
@@ -27,6 +29,17 @@ export class ListagemFilmesComponent implements OnInit {
       texto: [''],
       genero: ['']
     });
+    
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+      this.texto = val;
+      this.resetarConsulta()
+    });
+    
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.genero = val;
+      this.resetarConsulta();
+    });
+    
     this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção científica', 'Comédia', 'Drama'];
     this.listarFilmes();
   }
@@ -37,6 +50,13 @@ export class ListagemFilmesComponent implements OnInit {
 
   private listarFilmes(): void {
     this.pagina++;
-    this.filmesService.listar(this.pagina, this.qtdePorPagina).subscribe((filmes) => this.filmes.push(...filmes));
+    this.filmesService.listar(this.pagina, this.qtdePorPagina, this.texto, this.genero)
+      .subscribe((filmes) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 }
